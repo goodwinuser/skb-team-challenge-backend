cf01c56aac77b6173e334414f52a8f6bb07ae26c
Simeon Zagaynov <devepper@gmail.com>
2023-05-22 19:41:11 +0300

    init commit

---
+++ src/main/java/com/example/backendskvteamch/BackendSkvTeamChApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendskvteamch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSkvTeamChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSkvTeamChApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendskvteamch/BackendSkvTeamChApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendskvteamch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkvTeamChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
