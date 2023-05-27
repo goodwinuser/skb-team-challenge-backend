089bfe604205510bfdf2b1096151174a3242da7c
Simeon Zagaynov <devepper@gmail.com>
2023-05-27 23:51:40 +0300

    new method

--- src/main/java/com/example/backendskvteamch/controllers/VacancyController.java
+++ src/main/java/com/example/backendskvteamch/controllers/VacancyController.java
@@ -1,7 +1,10 @@
 package com.example.backendskvteamch.controllers;
 
 import com.example.backendskvteamch.entities.DTO.Vacancies.VacancyInfoDTO;
+import com.example.backendskvteamch.entities.Vacancies.Vacancy;
 import com.example.backendskvteamch.services.VacancyService;
+import com.example.backendskvteamch.utilities.annotations.AdminAuth;
+import com.example.backendskvteamch.utilities.annotations.UserAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +21,22 @@ import java.util.stream.Collectors;
 public class VacancyController {
     private final VacancyService vacancyService;
 
+    @AdminAuth
+    @UserAuth
     @GetMapping
     public ResponseEntity<List<VacancyInfoDTO>> getAllVacancies() {
         return ResponseEntity.ok(vacancyService.getVacancies().stream().map(VacancyInfoDTO::new).collect(Collectors.toList()));
     }
 
+    @AdminAuth
+    @UserAuth
+    @GetMapping("/open")
+    public ResponseEntity<List<VacancyInfoDTO>> getAllOpenVacancies() {
+        return ResponseEntity.ok(vacancyService.getVacancies().stream().filter(Vacancy::getIsOpen).map(VacancyInfoDTO::new).collect(Collectors.toList()));
+    }
+
+    @AdminAuth
+    @UserAuth
     @GetMapping("/{id}")
     public ResponseEntity<VacancyInfoDTO> getVacancy(@PathVariable Long id) {
         return ResponseEntity.ok(new VacancyInfoDTO(vacancyService.getVacancy(id)));
